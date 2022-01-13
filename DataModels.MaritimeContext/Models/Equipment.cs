96b9022fc698157a974d979ea5fb441f05ac380e
jonasmwahl <jonas.wahl@raalabs.com>
2022-01-13 22:12:42 +0100

    CustomFields as list

--- DataModels.MaritimeContext/Models/Equipment.cs
+++ DataModels.MaritimeContext/Models/Equipment.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
+
 namespace RaaLabs.DataModels.MaritimeContext
 {
     public class Equipment
     {
         public string Key { get; set; }
         public string Title { get; set; }
-        public CustomFields CustomFields { get; set; }
+        public List<CustomFields> CustomFields { get; set; }
 
     }
 }
