a7a09dff68be8624aa6903b100ff8881b04baf20
srudaa <sigbjorn.rudaa@raalabs.com>
2022-06-08 19:22:42 +0200

    Update version

--- DataModels.MaritimeContext/Models/EventData.cs
+++ DataModels.MaritimeContext/Models/EventData.cs
@@ -5,6 +5,7 @@ namespace RaaLabs.DataModels.MaritimeContext
         public string McKey { get; set; }
         public string Discipline { get; set; }
         public string EventType { get; set; }
+        public string EventTypeVersion { get; set; }
         public string TimestampUTC { get; set; }
         public string TimestampLocal { get; set; }
         public string Id { get; set; }
